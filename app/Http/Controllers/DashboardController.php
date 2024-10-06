e5c095cd73cea17b64e646f5daf4685f91242197
FarisFadhillah <qqfaris@gmail.com>
2024-10-06 20:43:39 +0700

    add: add inventaris table on user dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Barang;
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 use App\Models\Kerusakan;
+use App\Models\Lokasi;
 use App\Models\Maintener;
+use DataTables;
 use Carbon\Carbon;
 
 class DashboardController extends Controller
@@ -22,10 +26,73 @@ class DashboardController extends Controller
             'eksekutor' => Maintener::get()->count(),
         ]);
 
+        if ($req->ajax()) {
 
+            $lokasi_id = $req->lokasi_id;
+            $kategori_id = $req->kategori_id;
+            $tahun = $req->tahun;
+            
+            $kerusakan = Kerusakan::where('status', '!=', 'selesai')->get()->pluck('barang_id');
+
+            $user = auth()->user();
+
+            $data = Barang::select('barangs.*')->with(['kategori', 'lokasi', 'user'])
+                ->when(isset($lokasi_id), function ($q) use ($lokasi_id) {
+                    return $q->where('lokasi_id', $lokasi_id);
+                })
+                ->when(isset($kategori_id), function ($q) use ($kategori_id) {
+                    return $q->where('kategori_id', $kategori_id);
+                })
+                ->when(isset($tahun), function ($q) use ($tahun) {
+                    return $q->where('tahun', $tahun);
+                })
+                ->where('user_id', $user->id)
+                ->whereNotIn('id', $kerusakan)
+                ->orderBy('tahun', 'DESC')
+                ->get();
+
+
+            return Datatables::of($data)
+                ->addIndexColumn()
+                ->addColumn('user', function($row) {
+                    return $row->user->name . ' - ' . $row->user->jabatan->nama; // Menampilkan nama user dan jabatan
+                })
+                ->addColumn('action', function($row){
+                    $btn = '<div class="dropdown">
+                        <button type="button" class="btn btn-outline-primary btn-sm dropdown-toggle" id="dropdown-default-outline-primary" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Aksi
+                        </button>
+                        <div class="dropdown-menu fs-sm" aria-labelledby="dropdown-default-outline-primary" style="">';
+                        $btn .= '<a class="dropdown-item" href="'. route('inventaris.edit', $row->id).'"><i class="si si-note me-1"></i>Ubah</a>';
+                        $btn .= '<a class="dropdown-item" href="javascript:void(0)" onclick="hapus('. $row->id.')"><i class="si si-trash me-1"></i>Hapus</a>';
+                    $btn .= '</div></div>';
+                    return $btn;
+                })
+                ->editColumn('created_at', function ($row) {
+                    return Carbon::parse($row->created_at)->translatedFormat('d F Y');
+                })
+                ->editColumn('status', function ($row) {
+                    if($row->status == 'tersedia'){
+                        return '<span class="badge bg-primary">Tersedia</span>';
+                    }else if($row->status == 'rusak'){
+                        return '<span class="badge bg-danger">Rusak</span>';
+                    }else if($row->status == 'diperbaiki'){
+                        return '<span class="badge bg-warning">Diperbaiki</span>';
+                    }
+                })
+                ->rawColumns(['action', 'status']) 
+                ->make(true);
+        }
+        
+        $tahun = Barang::orderBy('tahun', 'ASC')->pluck('tahun')->unique(); // Ambil tahun unik
+        $kategori = Kategori::orderBy('nama', 'ASC')->get();
+        $lokasi = Lokasi::orderBy('nama', 'ASC')->get();
 
         return view('dashboard',[
             'ovr' => $ovr,
+            'kategori' => $kategori,
+            'lokasi' => $lokasi,
+            'tahun' => $tahun,
         ]);
     }
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -109,6 +109,84 @@
                     </div>
                 </a>
             </div>
+
+            <div class="content">
+
+                <div class="content-heading d-flex justify-content-between align-items-center">
+        
+                    <span>Data Inventaris</span>       
+                </div>
+        
+                <div class="block block-rounded">
+                    <div class="block-content p-3">
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="mb-4">
+                                    <label for="field-kategori_id">Filter Kategori</label>
+                                    <select class="form-select {{ $errors->has('kategori_id') ? 'is-invalid' : '' }}"
+                                        id="field-kategori_id" style="width: 100%;" name="kategori_id"
+                                        data-placeholder="Pilih Kategori">
+                                        <option></option>
+                                        @foreach ($kategori as $p)
+                                        <option value="{{ $p->id }}"  {{ (old('kategori_id') == $p->id) ? 'selected="selected"' : '' }}>{{ $p->nama }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        
+                            <div class="col-4">
+                                <div class="mb-4">
+                                    <label for="field-lokasi_id">Filter Lokasi</label>
+                                    <select class="form-select {{ $errors->has('lokasi_id') ? 'is-invalid' : '' }}"
+                                        id="field-lokasi_id" style="width: 100%;" name="lokasi_id"
+                                        data-placeholder="Pilih Lokasi">
+                                        <option></option>
+                                        @foreach ($lokasi as $p)
+                                        <option value="{{ $p->id }}" {{ (old('lokasi_id') == $p->id) ? 'selected="selected"' : '' }}>{{ $p->nama }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        
+                            <div class="col-4">
+                                <div class="mb-4">
+                                    <label for="field-tahun">Filter Tahun</label>
+                                    <select class="form-select {{ $errors->has('tahun') ? 'is-invalid' : '' }}"
+                                        id="field-tahun" style="width: 100%;" name="tahun"
+                                        data-placeholder="Pilih Tahun">
+                                        <option></option>
+                                        @foreach ($tahun as $p)
+                                        <option value="{{ $p }}" {{ (old('tahun') == $p) ? 'selected="selected"' : '' }}>{{ $p }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        
+        
+                        <table class="table table-bordered datatable w-100">
+                            <thead>
+                                <tr>
+                                    <th width="60px">No</th>
+                                    <th>No Inventaris</th>
+                                    <th>Nama Barang</th>
+                                    <th>Deskripsi</th>
+                                    <th>Kategori</th>
+                                    <th>Lokasi</th>
+                                    <th>Pengguna</th>
+                                    <th>Tahun</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+        
+                    </div>
+        
+                </div>
+        
+            </div>
             <!-- END Row #1 -->
         </div>
         @endif
@@ -136,6 +214,64 @@
                 window.location.href = "{{ route('dashboard') }}"+ `?start=${val[0]}&end=${val[1]}`;
             }
         });
+
+        $('#field-tahun').select2({
+            allowClear: true,
+        });
+        $('#field-lokasi_id').select2({
+        allowClear: true,
+        });
+        $('#field-kategori_id').select2({
+            allowClear: true,
+        });
+        
+        $("#field-lokasi_id").on("change", function(){
+            table.draw();
+        });
+        $("#field-tahun").on("change", function(){
+            table.draw();
+        });
+        
+        $("#field-kategori_id").on("change", function(){
+            table.draw();
+        });
+
+        $("#field-tgl").flatpickr({
+            altInput: true,
+            altFormat: "j F Y",
+            dateFormat: "Y-m-d",
+            locale: "id",
+            defaultDate: new Date(), // Default ke hari ini
+            mode: "single" // Hanya memilih satu tanggal
+        });
+
+
+
+        var table = $('.datatable').DataTable({
+        processing: true,
+        serverSide: true,
+        dom: "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>><'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+        ajax: {
+            url: "{{ route('dashboard') }}",
+            data: function(data) {
+                data.tahun = $('#field-tahun').val(); // Kirim tahun ke server
+                data.lokasi_id = $('#field-lokasi_id').val();
+                data.kategori_id = $('#field-kategori_id').val();
+                data.status = $('#field-status').val();
+            }
+        },
+        columns: [
+            {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+            {data: 'nomor', name: 'nomor'},
+            {data: 'nama', name: 'nama'},
+            {data: 'deskripsi', name: 'deskripsi'},
+            {data: 'kategori.nama', name: 'kategori.nama'},
+            {data: 'lokasi.nama', name: 'lokasi.nama'},
+            {data: 'user', name: 'user'},
+            {data: 'tahun', name: 'tahun'},
+            {data: 'status', name: 'status'},
+        ]
+    });
 
     </script>
     @endpush
